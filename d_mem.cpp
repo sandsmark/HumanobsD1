60adc088e8419f4fd82be2ccb48627550282fb22
eric@ru.is <eric@ru.is@d7b0e06d-8ebe-4c81-9feb-79243043a014>
2011-03-29 14:13:17 +0000

    git-svn-id: http://cadia.ru.is/svn/repos/HumanobsD1@8 d7b0e06d-8ebe-4c81-9feb-79243043a014

--- d_mem.cpp
+++ d_mem.cpp
@@ -48,28 +48,88 @@ void	DMem::eject(Code	*command){
 	if(function==r_exec::GetOpcode("speak")){
 
 		Speak	*speak=new	Speak();
-		uint16	args_set_index=command->code(CMD_ARGS),asIndex();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 		uint16	word_index=args_set_index+1;
-		uint16	deadline_index=args_set_index+2;
+		
 		std::string	w=r_code::Utils::GetString<Code>(command,word_index);
 		memcpy(speak->word,w.c_str(),w.length());
+		
+		uint16	deadline_index=args_set_index+2;
 		speak->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
 		NODE->send(module,speak,N::PRIMARY);
 	}else	if(function==r_exec::GetOpcode("move_hand")){
 
+		MoveTo	*move_to=new	MoveTo();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 
+		uint16	hand_index=args_set_index+1;
+		move_to->OID=command->get_reference(command->code(hand_index).asIndex())->getOID();
+
+		uint16	target_position_index=command->code(args_set_index+2).asIndex();
+		move_to->target_position[0]=command->code(target_position_index+1).asFloat();
+		move_to->target_position[1]=command->code(target_position_index+2).asFloat();
+		move_to->target_position[2]=command->code(target_position_index+3).asFloat();
+
+		uint16	deadline_index=args_set_index+3;
+		move_to->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
+		NODE->send(module,move_to,N::PRIMARY);
 	}else	if(function==r_exec::GetOpcode("grab_hand")){
 
+		Grab	*grab_hand=new	Grab();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 
+		uint16	hand_index=args_set_index+1;
+		grab_hand->OID=command->get_reference(command->code(hand_index).asIndex())->getOID();
+
+		uint16	deadline_index=args_set_index+2;
+		grab_hand->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
+		NODE->send(module,grab_hand,N::PRIMARY);
 	}else	if(function==r_exec::GetOpcode("release_hand")){
 
+		Release	*release_hand=new	Release();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 
+		uint16	hand_index=args_set_index+1;
+		release_hand->OID=command->get_reference(command->code(hand_index).asIndex())->getOID();
+
+		uint16	deadline_index=args_set_index+2;
+		release_hand->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
+		NODE->send(module,release_hand,N::PRIMARY);
 	}else	if(function==r_exec::GetOpcode("point_at")){
 
+		PointAt	*point_at=new	PointAt();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 
+		uint16	hand_index=args_set_index+1;
+		point_at->OID=command->get_reference(command->code(hand_index).asIndex())->getOID();
+
+		uint16	target_position_index=command->code(args_set_index+2).asIndex();
+		point_at->target_position[0]=command->code(target_position_index+1).asFloat();
+		point_at->target_position[1]=command->code(target_position_index+2).asFloat();
+		point_at->target_position[2]=command->code(target_position_index+3).asFloat();
+
+		uint16	deadline_index=args_set_index+3;
+		point_at->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
+		NODE->send(module,point_at,N::PRIMARY);
 	}else	if(function==r_exec::GetOpcode("look_at")){
 
+		LookAt	*look_at=new	LookAt();
+		uint16	args_set_index=command->code(CMD_ARGS).asIndex();
 
+		uint16	target_position_index=command->code(args_set_index+1).asIndex();
+		look_at->target_position[0]=command->code(target_position_index+1).asFloat();
+		look_at->target_position[1]=command->code(target_position_index+2).asFloat();
+		look_at->target_position[2]=command->code(target_position_index+3).asFloat();
+
+		uint16	deadline_index=args_set_index+2;
+		look_at->deadline=Utils::GetTimestamp<Code>(command,deadline_index);
+
+		NODE->send(module,look_at,N::PRIMARY);
 	}
 
 	//	Add more command procesing here.
